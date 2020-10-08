9b60ce4e03f9cbc4ff1d1a46d2d5d17df6ed3e71
hires <shire@unomaha.edu>
2020-10-08 15:08:28 -0500

    Added Home Page

--- Tech Exercise/src/DBConnection.java
+++ Tech Exercise/src/DBConnection.java
@@ -1,6 +1,9 @@
 
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -37,5 +40,54 @@ public class DBConnection extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+	
+	static Connection connection = null;
+
+	static void getDBConnection() {
+		System.out.println("-------- MySQL JDBC Connection Testing ------------");
+	    try {
+	    	Class.forName("com.mysql.jdbc.Driver");
+	    } catch (ClassNotFoundException e) {
+	         System.out.println("Where is your MySQL JDBC Driver?");
+	         e.printStackTrace();
+	         return;
+	      }
+	    System.out.println("MySQL JDBC Driver Registered!");
+
+	    connection = null;
+	    try {
+	        UtilProp.loadProperty();
+	        connection = DriverManager.getConnection(getURL(), getUserName(), getPassword());
+	    } catch (Exception e) {
+	         System.out.println("Connection Failed! Check output console");
+	         e.printStackTrace();
+	         return;
+	    }
+
+	    if (connection != null) {
+	       System.out.println("You made it, take control your database now!");
+	    } else {
+	       System.out.println("Failed to make connection!");
+	    }
+	}
+	   
+	
+	static String getURL() {
+		String url = UtilProp.getProp("url");
+		System.out.println("[DBG] URL: " + url);
+		return url;
+	}
+
+	static String getUserName() {
+		String usr = UtilProp.getProp("user");
+		System.out.println("[DBG] Username: " + usr);
+		return usr;
+	}
+
+	static String getPassword() {
+		String pwd = UtilProp.getProp("password");
+		System.out.println("[DBG] Password: " + pwd);
+		return pwd;
+   }
 
 }
